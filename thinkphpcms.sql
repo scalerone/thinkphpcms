9f4a6c896d83fa64753651b5841fbd2e15aae0f9
TankCJZ <cjztan@foxmail.com>
2017-02-22 17:42:42 +0800

    2017/2/22 PM 5:41

---
+++ thinkphpcms.sql
@@ -0,0 +1,49 @@
+-- phpMyAdmin SQL Dump
+-- version phpStudy 2014
+-- http://www.phpmyadmin.net
+--
+-- 主机: localhost
+-- 生成日期: 2017 �?02 �?22 �?17:42
+-- 服务器版本: 5.5.47
+-- PHP 版本: 5.5.30
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- 数据库: `thinkphpcms`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `cms_admin`
+--
+
+CREATE TABLE IF NOT EXISTS `cms_admin` (
+  `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL DEFAULT '',
+  `password` char(32) NOT NULL DEFAULT '',
+  `email` varchar(50) DEFAULT NULL,
+  `lastlogintime` int(10) DEFAULT NULL,
+  `lastloginip` varchar(20) DEFAULT '',
+  `status` smallint(1) NOT NULL DEFAULT '1' COMMENT '1:正常0:锁定',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- 转存表中的数据 `cms_admin`
+--
+
+INSERT INTO `cms_admin` (`id`, `username`, `password`, `email`, `lastlogintime`, `lastloginip`, `status`) VALUES
+(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', NULL, 1487756359, '127.0.0.1', 1);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
